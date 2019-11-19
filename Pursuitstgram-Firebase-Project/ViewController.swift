a3510ee7a93780a8e62a46752d9c001a2cd83466
Eric Widjaja <ericwidjaja@pursuit.org>
2019-11-18 22:07:45 -0500

    running test, change bckgrnd color to green, all pods are successfully downloaded, VC is working

--- Pursuitstgram-Firebase-Project/ViewController.swift
+++ Pursuitstgram-Firebase-Project/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
         // Do any additional setup after loading the view.
     }
 
