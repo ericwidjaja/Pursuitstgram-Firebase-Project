05825c830695941e2302592949ed0f68463f0430
Eric Widjaja <ericwidjaja@pursuit.org>
2019-11-23 21:40:32 -0500

    adjusted objects in LogInView and LogInVC

--- Pursuitstgram-Firebase-Project/Controllers/LogInVC.swift
+++ Pursuitstgram-Firebase-Project/Controllers/LogInVC.swift
@@ -9,11 +9,25 @@ import UIKit
 import FirebaseAuth
 
 class LogInVC: UIViewController {
+    
+    var logInEmail: UITextField?
+    var logInPassword: UITextField?
+    var loginView = LogInView()
+    
+    
+    
+    private func setLogInView() {
+        view.addSubview(loginView)
+        
+        self.view.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+        
+    }
 
+//MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
-        // Do any additional setup after loading the view.
+        setLogInView()
+        
     }
 
 
--- Pursuitstgram-Firebase-Project/Views/LogInView.swift
+++ Pursuitstgram-Firebase-Project/Views/LogInView.swift
@@ -12,12 +12,14 @@ import FirebaseAuth
 
 class LogInView: UIView {
 
+//MARK: - Objects
     var projectTitle: UILabel = {
         let label = UILabel()
-        label.numberOfLines = 0
+        label.numberOfLines = 1
         label.text = "P U R S U I T S T G R A M"
-        label.font = UIFont(name: "Noteworthy-Bold", size: 28)
+        label.font = UIFont(name: "Noteworthy-Bold", size: 32)
         label.textColor = UIColor.white
+        label.adjustsFontSizeToFitWidth = true
         label.backgroundColor = .clear
         label.textAlignment = .center
         return label
@@ -41,7 +43,57 @@ class LogInView: UIView {
         textField.isSecureTextEntry = true
         return textField
     }()
-
     
-
+    lazy var objectsViewArray = [self.projectTitle, self.emailTxtField, self.passwordTxtField]
+    
+    //MARK:Add ViewsToSubviews
+    func addViewsToSubView() {
+        for aView in objectsViewArray {
+            self.addSubview(aView)
+            aView.translatesAutoresizingMaskIntoConstraints = false
+        }
+    }
+    
+//MARK: Constraints
+    func setConstraints() {
+        addViewsToSubView()
+        titleConstraints()
+//        emailTextFieldConstraints()
+//        passwordTextFieldConstraints()
+    }
+    
+    private func titleConstraints() {
+        addSubview(projectTitle)
+        NSLayoutConstraint.activate([
+        projectTitle.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+        projectTitle.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor,constant: 60),
+        projectTitle.heightAnchor.constraint(equalToConstant: 70), projectTitle.widthAnchor.constraint(equalToConstant: 350)])
+    }
+    
+//    private func emailTextFieldConstraints() {
+//        emailTxtField.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            emailTxtField.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor,constant: 120),
+//            emailTxtField.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+//            emailTxtField.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,constant: 20),
+//            emailTxtField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor,constant: -20)])
+//    }
+//    private func passwordTextFieldConstraints() {
+//        passwordTxtField.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            passwordTxtField.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor),
+//            passwordTxtField.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor,constant: 150),
+//            passwordTxtField.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor,constant: 20),
+//            passwordTxtField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor,constant: -20)])
+//    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        setConstraints()
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
 }
