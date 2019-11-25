7b77a930e6c3aff39a713c4ff31b6a129ea1e428
Eric Widjaja <ericwidjaja@pursuit.org>
2019-11-24 21:20:03 -0500

    added AppUser file and FirebaseTabBarVC

---
+++ Pursuitstgram-Firebase-Project/Controllers/FirebaseTabBarVC.swift
@@ -0,0 +1,30 @@
+//
+//  FirebaseTabBarVC.swift
+//  Pursuitstgram-Firebase-Project
+//
+//  Created by Eric Widjaja on 11/24/19.
+//  Copyright © 2019 Eric.W. All rights reserved.
+//
+
+import UIKit
+
+class FirebaseTabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pursuitstgram-Firebase-Project/Models/AppUser.swift
@@ -0,0 +1,9 @@
+//
+//  AppUser.swift
+//  Pursuitstgram-Firebase-Project
+//
+//  Created by Eric Widjaja on 11/24/19.
+//  Copyright © 2019 Eric.W. All rights reserved.
+//
+
+import Foundation
